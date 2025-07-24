4335143c41a671bcad184ef2465a065beb241ba0
Marc Kalil <marc@marckalil.com>
2025-07-24 16:25:28 +0200

    replace database by UsersRepository in UsersService

--- begin/packages/backend/src/modules/users/usersService.ts
+++ begin/packages/backend/src/modules/users/usersService.ts
@@ -1,49 +1,57 @@
-import { Database } from "@dddforum/backend/src/shared/database";
+import { User, ValidatedUser } from "@dddforum/shared/src/api/users";
+import { TextUtil } from "@dddforum/shared/src/utils/textUtils";
+
+import { TransactionalEmailAPI } from "../notifications/transactionalEmailAPI";
+
 import { CreateUserCommand } from "./usersCommand";
 import {
   EmailAlreadyInUseException,
   UserNotFoundException,
   UsernameAlreadyTakenException,
 } from "./usersExceptions";
-import { User } from "@dddforum/shared/src/api/users";
-import { TransactionalEmailAPI } from "../notifications/transactionalEmailAPI";
+import { UsersRepository } from "./ports/usersRepository";
 
 export class UsersService {
   constructor(
-    private db: Database,
+    private repository: UsersRepository,
     private emailAPI: TransactionalEmailAPI,
   ) {}
 
   async createUser(userData: CreateUserCommand): Promise<User> {
-    const existingUserByEmail = await this.db.users.findUserByEmail(
+    const existingUserByEmail = await this.repository.findByEmail(
       userData.email,
     );
     if (existingUserByEmail) {
       throw new EmailAlreadyInUseException(userData.email);
     }
 
-    const existingUserByUsername = await this.db.users.findUserByUsername(
+    const existingUserByUsername = await this.repository.findByUsername(
       userData.username,
     );
     if (existingUserByUsername) {
       throw new UsernameAlreadyTakenException(userData.username);
     }
 
-    const { password, ...user } = await this.db.users.save(userData);
+    const validatedUser: ValidatedUser = {
+      ...userData.props,
+      password: TextUtil.createRandomText(10),
+    };
+
+    const user = await this.repository.save(validatedUser);
 
     await this.emailAPI.sendMail({
       to: user.email,
       subject: "Your login details to DDDForum",
       text: `Welcome to DDDForum. You can login with the following details </br>
       email: ${user.email}
-      password: ${password}`,
+      password: ${user.password}`,
     });
 
     return user;
   }
 
   async getUserByEmail(email: string) {
-    const user = await this.db.users.findUserByEmail(email);
+    const user = await this.repository.findByEmail(email);
     if (!user) {
       throw new UserNotFoundException(email);
     }
